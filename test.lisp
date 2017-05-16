0a3c98877c52b0d08a52f169e5b61b54b17a37ac
fireflower0 <fireflower0@hotmail.co.jp>
2017-05-16 23:00:21 +0900

    10×10の表を追加

--- test.lisp
+++ test.lisp
@@ -1,2 +1,9 @@
+(defun multiple-table ()
+  (dotimes (i 10)
+    (dotimes (j 10)
+      (format t "~3d " (* (1+ i) (1+ j))))
+    (format t "~%")))
+
 (defun main ()
-	(format t "Hello, world\n"))
+  (format t "Hello, world!~%")
+  (multiple-table))
